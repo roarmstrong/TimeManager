fa1f68b40fab86984c11add90aa9f2fa6768748d
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-02-06 12:23:11 +1100

    more testing

--- test/test_functionality.py
+++ test/test_functionality.py
@@ -160,8 +160,6 @@ class TestForLayersWithOnePointPerSecond(TestWithQGISLauncher):
         self.assertEquals(self.tlm.getCurrentTimePosition(), expected_datetime)
         self.tlm.setTimeFrameSize(2)
         self.assertEquals(layer.featureCount(), 2)
-        del settings.layer
-        del timeLayer
 
 
 class testTimeManagerWithoutGui(TestWithQGISLauncher):
